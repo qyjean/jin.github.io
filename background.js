2823b655f5cd018e12aa4841ce6137d3d053f259
qyjean <99635933+qyjean@users.noreply.github.com>
2023-03-06 03:49:04 +0900

    최종

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ const images = ["0.png", "1.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `${chosenImage}`;
 document.body.appendChild(bgImage);
 
